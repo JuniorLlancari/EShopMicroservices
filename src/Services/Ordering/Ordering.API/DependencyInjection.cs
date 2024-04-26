f6a3c51850247541354ee9b8c2ba708ab0607d8b
Junior LLancari <64571671+JuniorLlancari@users.noreply.github.com>
2024-04-26 00:18:27 -0500

    Ordering - Application

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+﻿using BuildingBlocks.Exceptions.Handler;
+using Carter;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace Ordering.API
 {
@@ -6,12 +9,22 @@ namespace Ordering.API
     {
         public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
         {
-             return services;
+            services.AddCarter();
+            services.AddExceptionHandler<CustomExceptionHandler>();
+            services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("Database")!);
+            return services;
         }
 
         public static WebApplication UseApiServices(this WebApplication app)
         {
-  
+            app.MapCarter();
+            app.UseExceptionHandler(options => { });
+            app.UseHealthChecks("/health",
+            new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
             return app;
         }
 
